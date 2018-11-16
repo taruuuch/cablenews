04768458a717bd282099d2c00ff046575038062f
Artemchuk Dmytro <taruuuch@gmail.com>
2018-11-16 18:07:03 +0200

    Start change gulp on webpack
    Add: 
    - slider
    - header
    - settings
    
    New:
    - Change gulp on webpack

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 'use strict';
 
 let browserSync  = require('browser-sync').create(),
-    gulp         = require('gulp'),
+    // gulp         = require('gulp'),
     pug          = require('gulp-pug'),
     sass         = require('gulp-sass'),
     cleancss     = require('gulp-clean-css'),
@@ -13,7 +13,9 @@ let browserSync  = require('browser-sync').create(),
     sourcemaps   = require('gulp-sourcemaps'),
     uglify       = require('gulp-uglify'),
     wait         = require('gulp-wait'),
-    reload       = browserSync.reload;
+	reload       = browserSync.reload;
+	
+const { task, watch, src, dist } = require('gulp');
 
 let config = {
 	dist: './dist/',
@@ -39,7 +41,7 @@ let config = {
 	}
 };
 
-gulp.task('browser-sync', ['style', 'template', 'script'], function () {
+task('browser-sync', ['style', 'template', 'script'], function () {
 	browserSync.init({
 		server: {
 			baseDir: config.dist
@@ -48,10 +50,10 @@ gulp.task('browser-sync', ['style', 'template', 'script'], function () {
 	});
 });
 
-gulp.task('style', function () {
-	return gulp.src([config.style.in])
+task('style', function () {
+	return src([config.style.in])
 		.pipe(wait(200))
-		.pipe(sass.sync())
+		.pipe(sass())
 		.on('error', notify.onError({
 				message: '\n<%= error.message %>',
 				title: 'SASS'
@@ -66,7 +68,7 @@ gulp.task('style', function () {
 			suffix: '.min'
 		}))
 		.pipe(sourcemaps.write('.'))
-		.pipe(gulp.dest(config.style.out))
+		.pipe(dest(config.style.out))
 		.pipe(
 			browserSync.reload({
 				stream: true
@@ -74,27 +76,27 @@ gulp.task('style', function () {
 		);
 });
 
-gulp.task('template', function () {
-	return gulp.src([
+task('template', function () {
+	return src([
 			config.template.in
 		])
-		.on('error', notify.onError({
-			message: '\n<%= error.message %>',
-			title: 'Template'
-		}))
 		.pipe(pug({
 			pretty: true
 		}))
-		.pipe(gulp.dest(config.template.out));
+		.on('error', notify.onError({
+			message: '\n<%= error.message %>',
+			title: 'TEMPLATE'
+		}))
+		.pipe(dest(config.template.out));
 });
 
-gulp.task('script', function () {
-	return gulp.src([
+task('script', function () {
+	return src([
 			config.script.in
 		])
 		.on('error', notify.onError({
 			message: '\n<%= error.message %>',
-			title: 'Scripts'
+			title: 'SCRIPTS'
 		}))
 		.pipe(sourcemaps.init())
 		.pipe(uglify())
@@ -102,7 +104,7 @@ gulp.task('script', function () {
 			suffix: '.min'
 		}))
 		.pipe(sourcemaps.write('.'))
-		.pipe(gulp.dest(config.script.out))
+		.pipe(dest(config.script.out))
 		.pipe(
 			browserSync.reload({
 				stream: true
@@ -110,9 +112,10 @@ gulp.task('script', function () {
 		);
 });
 
-gulp.task('js-lib', function () {
-	return gulp.src([
+task('js-lib', function () {
+	return src([
 			'./node_modules/jquery/dist/jquery.min.js',
+			'./node_modules/bootstrap/dist/js/bootstrap.min.js',
 			'./node_modules/owl.carousel/dist/owl.carousel.min.js'
 		])
 		.pipe(gulp.dest(config.script.out + '/lib/'))
@@ -123,11 +126,12 @@ gulp.task('js-lib', function () {
 		);
 });
 
-gulp.task('css-lib', function () {
-	return gulp.src([
+task('css-lib', function () {
+	return src([
+			'./node_modules/bootstrap/dist/css/bootstrap.min.css',
 			'./node_modules/owl.carousel/dist/assets/owl.carousel.min.css'
 		])
-		.pipe(gulp.dest(config.style.out + '/lib/'))
+		.pipe(dest(config.style.out + '/lib/'))
 		.pipe(
 			browserSync.reload({
 				stream: true
@@ -135,14 +139,19 @@ gulp.task('css-lib', function () {
 		);
 });
 
-gulp.task('image', function () {
-	return gulp.src(config.image.in)
+task('image', function () {
+	return src(config.image.in)
 		.pipe(imagemin({
 			optimizationLevel: 5,
-			progressive: true,
-			interlaced: true
+			progressive      : true,
+			interlaced       : true,
+			svgoPlugins      : [
+				{
+					removeViewBox: true
+				}
+			]
 		}))
-		.pipe(gulp.dest(config.image.out))
+		.pipe(dest(config.image.out))
 		.pipe(
 			browserSync.reload({
 				stream: true
@@ -150,24 +159,24 @@ gulp.task('image', function () {
 		);
 });
 
-gulp.task('font', function () {
-	return gulp.src([
+task('font', function () {
+	return src([
 			config.font.in
 		])
-		.pipe(gulp.dest(config.font.out));
+		.pipe(dest(config.font.out));
 });
 
-gulp.task('clean', function () {
+task('clean', function () {
 	return del.sync(config.dist);
 });
 
-gulp.task('watch', function () {
-	gulp.watch([config.watch.style], ['style']).on("change", reload)
-	gulp.watch([config.watch.template], ['template']).on("change", reload)
-	gulp.watch([config.script.in], ['script']).on("change", reload)
-	gulp.watch([config.image.in], ['image']).on("change", reload);
+task('watch', function () {
+	watch([config.watch.style], ['style']).on("change", reload)
+	watch([config.watch.template], ['template']).on("change", reload)
+	watch([config.script.in], ['script']).on("change", reload)
+	watch([config.image.in], ['image']).on("change", reload);
 });
 
-gulp.task('default', ['clean'], function () {
+task('default', ['clean'], function () {
 	gulp.start('script', 'js-lib', 'css-lib', 'image', 'font', 'watch', 'browser-sync');
 });
--- src/script/app.js
+++ src/script/app.js
@@ -1,18 +1,30 @@
 (function() {
-    $('.news-slider').owlCarousel({
-        loop: true,
-        items: 1,
-        nav: true,
-        autoplay: true,
+    $('.slider__news').owlCarousel({
+        loop              : true,
+        items             : 1,
+        dots              : false,
+        navText           : [
+            "<img src='img/icon/arrow-left.svg' />",
+            "<img src='img/icon/arrow-right.svg' />",
+        ],
+        autoplay          : true,
         autoplayHoverPause: true,
-        autoplaySpeed: 7000,
-        animateOut: 'fadeOut',
-        animateIn: 'fadeIn',
-        smartSpeed: 200
+        autoplaySpeed     : 7000,
+        animateOut        : 'fadeOut',
+        animateIn         : 'fadeIn',
+        smartSpeed        : 200,
+        responsive        : {
+            0   : {
+                nav       : false
+            }, 
+            840 : {
+                nav       : true
+            }
+        }
     });
 
     $('.open-search-form').click(function() {
-        $('.search-form').slideToggle().show();
+        $('.search-form').slideToggle().css('display', 'flex');
         $(this).hide();
     });
 })();
